8d05b90ed0f8a47953babb0c2407d10f14d8bc6a
kc-clintone <kayseeclintone@gmail.com>
2026-04-14 08:38:28 +0300

    feat: add a function to translate text

--- chichwa/translator/views.py
+++ chichwa/translator/views.py
@@ -1,3 +1,26 @@
+import json
+from django.http import JsonResponse
 from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
+from .translator import translate
 
-# Create your views here.
+
+def home(request):
+    return render(request, "index.html")
+
+
+@csrf_exempt
+def translate_text(request):
+    if request.method == "POST":
+        data = json.loads(request.body)
+
+        text = data.get("text", "")
+        target = data.get("targetLanguage", "sw")
+
+        translated = translate(text, target)
+
+        return JsonResponse({
+            "translatedText": translated
+        })
+
+    return JsonResponse({"error": "POST request required"}, status=400)
